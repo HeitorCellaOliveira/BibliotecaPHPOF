9cb82c1276766e15b7cdd6be18deb8c701cb034b
Helena Gomes <87838594+lenagomesz@users.noreply.github.com>
2023-08-09 11:27:58 -0300

    Update loginPage.php

--- loginPage.php
+++ loginPage.php
@@ -46,10 +46,12 @@ if (isset($_POST['nomeUsuario']) && isset($_POST['senha'])) {
     <title>Login | Mascarenhas</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v6.4.0/css/all.css">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Philosopher:ital@1&family=Playfair+Display:wght@600&family=Ysabeau+Infant:ital,wght@1,500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=El+Messiri:wght@700&family=Philosopher:ital@1&family=Playfair+Display:wght@600&family=Ysabeau+Infant:ital,wght@1,500&display=swap" rel="stylesheet">
     <title>Início | Mascarenhas</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <style>
@@ -61,8 +63,14 @@ if (isset($_POST['nomeUsuario']) && isset($_POST['senha'])) {
         padding: 0;
         text-decoration: none;
         text-transform: none;
+        
     }
-
+    body {
+            background-color: #5C5C5C; /* Light gray background color */
+            margin: 0;
+            padding: 0;
+        }
+        
     .logo{
       left:1%;
       position: absolute;
@@ -143,23 +151,28 @@ if (isset($_POST['nomeUsuario']) && isset($_POST['senha'])) {
         margin-bottom: 0;
     }
 
-    .ret{
-        height: 600px;
-        width:700px ;
-        background:#C9C9C9;
-        position: absolute;
-        left:45%;
-        top:200px;
-    }
+    
     .ret2{
         height: 600px;
-        width:500px ;
-        background:#000000;
+        width:700px ;
         position: absolute;
-        right:100%;
-        top: 0px;
+        left:14%;
+        top: 87.3px;
+        border-radius: 10px;
+        
+    }
+    .ret{
+        height: 600px;
+        width:450px ;
+        background: #FFFFFF;
+        position: absolute;
+        left:55%;
+        top:175px;
+        
+        border-radius: 0px 10px 10px 0px;
 
     }
+
 
 
     footer {
@@ -194,46 +207,131 @@ if (isset($_POST['nomeUsuario']) && isset($_POST['senha'])) {
         text-decoration: none;
     }
 
+    .tittle{
+        color:#000000;
+        font-size:50px;
+        font-family: 'Playfair Display', serif;
+        position: absolute;
+        top:75px;
+        left:75px;
+    }
+    #user{
+        position: absolute;
+        top: 210px; 
+        left: 35px; 
+        font-size: 24px;
+        color:;
+        z-index: 1;
+    }
+
+
+    #nomeUsuario{
+    position: absolute;
+	width:400px;
+	height:40px;
+    border-radius: 20px;
+    margin-bottom: 20px;
+    padding-left: 45px;
+    top:200px;
+    left:20px;
+
+    }
+    #user2{
+    position: absolute;
+    top: 290px; 
+    left: 35px; 
+    font-size: 24px;
+    color:;
+    z-index: 1;
+
+    }
+    #senha{
+    position: absolute;
+	width:400px;
+	height:40px;
+    top:280px;
+    left:20px;
+    border-radius: 20px;
+    margin-bottom: 20px;
+    padding-left: 45px;
+
+    }
+
+    .button {
+    font-size:20px;
+    font-family:'Bebas', serif;
+    outline:none;
+    height: 50px;
+    text-align: center;
+    width: 250px;
+    border-radius:40px;
+    background: #fff;
+    border: 4px solid $green;
+    color:$green;
+    letter-spacing:1px;
+    text-shadow:0;
+    font-size:12px;
+    weight:bold;
+    position: relative;
+    top:100px;
+    left:0%;
+    text-decoration:bold;
+  }
+  .naopossui{
+    color:#000000;
+    position:absolute;
+    font-family: 'Bebas Neue', sans-serif;
+    top:330px;
+    font-size:13px;
+    left:65%;
+  }
+ 
+
     </style>
 </head>
 <body>
     <nav class="navbar">
-            <img src="images/logo.png" class="logo">
+            <img src="Imagens/logo.png" class="logo">
             </div>
     </nav>
+    <center>
+ 
+
+    <div class="ret2">
+        <img src="Imagens/black.png" class="ret2">
+    </div>
     <div class="ret">
+    
+
+
     
         <center>
         <form method="post" action="">
         <table style="margin-top: 20em;">
             <tr>
-                <td><h1>Acesse sua conta!</h1></td>
+                <td><p class="tittle">Login</p></td>
             </tr>
-
-            <tr>
-                <td><label for="">Nome de Usuário:</label></td>
-            </tr>
-
+            <i class="fa-solid fa-user" id="user"></i>
+            
             <tr>
                 <td><input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Digite o usuário"></td>
             </tr>
 
-            <tr>
-                <td><br><label for="">Senha:</label></td>
-            </tr>
-
+            <i class="fa-solid fa-lock" id="user2"></i>
             <tr> 
                 <td><input type="password" name="senha" id="senha" placeholder="Digite a senha"></td>
             </tr>
 
             <tr>
-                <td><br><input type="submit" value="Entrar" style="cursor: pointer;"><a href="cadastroPage.php" class="naopossui">Não possui conta?</a></td>
+                <td><br><input type="submit" value="Entrar" style="cursor: pointer;" class="button">
+                
+                <a href="cadastroPage.php" class="naopossui">Não possui conta?</a></td>
             </tr>
+            
         </table>
         </form>
     </center>
-    <div class="ret2">
-        <img src="logo.png">
-    
+    </div>
+
 </body>
 </html>
